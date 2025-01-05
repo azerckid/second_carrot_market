a60dff97c6531e2c4386a8fc41de9cde4e7d9286
남형석 <namhyeongseog@namhyeongseogs-MacBook-Pro.local>
2025-01-05 15:09:14 +0900

    practice tailwind

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,25 @@
 export default function Home() {
-  return <div></div>;
+  return (
+    <main className="h-screen flex items-center justify-center bg-gray-100 p-4">
+      <div className="w-96 p-4 rounded-md bg-white shadow-md">
+        <div>
+          <div className="flex flex-col justify-between">
+            <span className="text-gray-400">In transit</span>
+            <span className="text-4xl font-semibold">Coolblue</span>
+          </div>
+        </div>
+        <div>
+          <span>07:00</span>
+          <span>post</span>
+        </div>
+        <div></div>
+        <div>
+          <span>Expected</span>
+          <span>Sorting center</span>
+          <span>In transit</span>
+          <span>Delivered</span>
+        </div>
+      </div>
+    </main>
+  );
 }
