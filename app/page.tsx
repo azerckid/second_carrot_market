4e73cdc7e6d7d6d23f36440c26cd6023f14c599f
남형석 <namhyeongseog@namhyeongseogs-MacBook-Pro.local>
2025-01-07 15:05:30 +0900

    done #3.2

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,29 @@
 export default function Home() {
   return (
     <main className="h-screen flex items-center justify-center bg-gray-100 p-4">
-      <div className="w-96 p-4 rounded-md bg-white shadow-md">
-        <div>
+      <div className="w-96 p-4 rounded-2xl bg-white shadow-md">
+        <div className="flex items-center justify-between">
           <div className="flex flex-col justify-between">
             <span className="text-gray-400">In transit</span>
             <span className="text-4xl font-semibold">Coolblue</span>
           </div>
+          <div className="size-12 rounded-full bg-orange-300"></div>
         </div>
-        <div>
-          <span>07:00</span>
-          <span>post</span>
+        <div className="my-4 flex items-center gap-2">
+          <span className="bg-green-400 p-2 px-3 rounded-3xl text-white uppercase text-sm font-bold">
+            Today
+          </span>
+          <span>09:30~10:30</span>
         </div>
-        <div></div>
-        <div>
+        <div className="relative">
+          <div className="w-full h-2 bg-gray-200 rounded-full absolute"></div>
+          <div className="w-2/3 h-2 bg-green-400 rounded-full absolute"></div>
+        </div>
+        <div className="flex justify-between items-center mt-6 text-gray-600">
           <span>Expected</span>
           <span>Sorting center</span>
           <span>In transit</span>
-          <span>Delivered</span>
+          <span className="text-gray-400">Delivered</span>
         </div>
       </div>
     </main>
